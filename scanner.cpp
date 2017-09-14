e2b2eb1cf3871e40b37fceb1e800713e9826b935
Murillo R <murdrof@gmail.com>
2017-09-14 15:39:02 -0300

    Cria função que transforma caractere em coluna

---
+++ scanner.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;  
+
+#define MAX_LINES 21
+#define MAX_COLS 15
+
+int sigma[MAX_LINES][MAX_COLS];
+
+int charToCol(char c) {
+    if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+        return 0;
+    } else if (c >= 48 && c <= 57) {
+        return 1;
+    } else if (c >= 40 && c <= 47) {
+        return c - 38;
+    } else if (c >= 60 && c <= 62) {
+        return c - 50;
+    } else if (c == 59) {
+        return 13;
+    } else {
+        return 14;
+    }
+}
+
+int main() {
+    char c = '/';
+    cout << charToCol(c) << endl;
+    return 0;
+}
