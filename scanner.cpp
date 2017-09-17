a4712c5567eb304c88bfa7f3c9a1da0eb5372dfc
Murillo R <murdrof@gmail.com>
2017-09-17 16:01:16 -0300

    Organização de código

--- scanner.cpp
+++ scanner.cpp
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
 
-using namespace std;  
+using namespace std;
 
-#define MAX_LINES 21
-#define MAX_COLS 15
+#define MAX_STATES 21
+#define MAX_CHARSET 15
 
-int sigma[MAX_LINES][MAX_COLS];
+int sigma[MAX_STATES][MAX_CHARSET];
 
-int charToCol(char c) {
-    if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
-        return 0;
-    } else if (c >= 48 && c <= 57) {
-        return 1;
-    } else if (c >= 40 && c <= 47) {
-        return c - 38;
-    } else if (c >= 60 && c <= 62) {
-        return c - 50;
-    } else if (c == 59) {
-        return 13;
-    } else {
-        return 14;
-    }
+inline int charToCol(char c) {
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+		return 0;
+	} else if (c >= 48 && c <= 57) {
+		return 1;
+	} else if (c >= 40 && c <= 47) {
+		return c - 38;
+	} else if (c >= 60 && c <= 62) {
+		return c - 50;
+	} else if (c == 59) {
+		return 13;
+	} else {
+		return 14;
+	}
 }
 
 int main() {
-    char c = '/';
-    cout << charToCol(c) << endl;
-    return 0;
+	char c = '/';
+	cout << charToCol(c) << endl;
+	return 0;
 }
