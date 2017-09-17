987754601c1163611931d3d2594437b6aca19695
Murillo R <murdrof@gmail.com>
2017-09-17 16:46:20 -0300

    Leitura de caractere por caractere do fonte

--- scanner.cpp
+++ scanner.cpp
@@ -24,7 +24,29 @@ inline int charToCol(char c) {
 }
 
 int main() {
-  char c = '/';
-  cout << charToCol(c) << endl;
+  char caracter, pause;
+
+  //Cria um fluxo de arquivo a partir de "texto.alg"
+  ifstream myfile ("texto.alg");
+
+  //Testa se foi possivel abrir o arquivo
+  if (myfile.is_open()) {
+    //Loop de leitura de caracteres
+    while (myfile.get(caracter)) {
+      if (caracter != '\n') {
+        //Imprime o caracter de uma linha
+        cout << caracter;
+      } else {
+        //Pausa a cada linha esperando um "enter"
+        cin.get(pause);
+      }
+    }
+    //Fecha o fluxo de arquivo
+    myfile.close();
+  }
+  //Se o arquivo n�o for aberto
+  else {
+    cout << "Impossivel abrir o arquivo!\n";
+  }
   return 0;
 }
