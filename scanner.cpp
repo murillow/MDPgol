d08f7e0448c667be141d295485d2489087204aa6
Murillo R <murdrof@gmail.com>
2017-09-17 16:11:36 -0300

    Converte indentação para espaços

--- scanner.cpp
+++ scanner.cpp
@@ -8,23 +8,23 @@ using namespace std;
 int sigma[MAX_STATES][MAX_CHARSET];
 
 inline int charToCol(char c) {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
-		return 0;
-	} else if (c >= 48 && c <= 57) {
-		return 1;
-	} else if (c >= 40 && c <= 47) {
-		return c - 38;
-	} else if (c >= 60 && c <= 62) {
-		return c - 50;
-	} else if (c == 59) {
-		return 13;
-	} else {
-		return 14;
-	}
+  if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+    return 0;
+  } else if (c >= 48 && c <= 57) {
+    return 1;
+  } else if (c >= 40 && c <= 47) {
+    return c - 38;
+  } else if (c >= 60 && c <= 62) {
+    return c - 50;
+  } else if (c == 59) {
+    return 13;
+  } else {
+    return 14;
+  }
 }
 
 int main() {
-	char c = '/';
-	cout << charToCol(c) << endl;
-	return 0;
+  char c = '/';
+  cout << charToCol(c) << endl;
+  return 0;
 }
